8656fa591eb198b3a7d5fd52129be1887fcd7a33
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-15 19:50:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const app = express()
 
+let name = "Dani";
+let oseba = "Učitelj";
+let učitelj = "Uroš";
+
 app.get("/", function(req,res){
-    res.send("WORKING!!!")
+
+    if ( oseba === "Uroš") {
+            res.send("Živjo Uroš");
+        } else {
+            res.send("Žal niste prava oseba")
+    }
+
 })
 
 app.listen(process.env.PORT || 5000)
