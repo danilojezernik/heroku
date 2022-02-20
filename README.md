258f72b0748a751ee5b7397e213306ec9c4a1b99
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-20 19:11:42 +0100

    Test test

---
+++ README.md
@@ -0,0 +1,7 @@
+# NODE.js express
+
+![alt text](nodejs.png "Bootstrap 5")
+
+![alt text](nodejsexpress.png "Bootstrap 5")
+
+![alt text](heroku.png "Bootstrap 5")
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-    res.send("WORKING!!!")
+        res.send('Deluje! Dogaja se! HAHA :D');
 })
 
 app.listen(process.env.PORT || 5000)
