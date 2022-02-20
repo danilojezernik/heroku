31e0e19502c34c213a060a7244e8479c37074f25
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-20 19:13:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req,res){
-        res.send('Deluje! Dogaja se! :D');
+        res.send('Deluje! Dogaja se! HAHfaA :D');
 })
 
 app.listen(process.env.PORT || 5000)
