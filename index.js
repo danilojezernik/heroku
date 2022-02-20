606c77f2c54048babed382adb72ad9c956cad6fe
Danilo <96477921+danilojezernik@users.noreply.github.com>
2022-02-20 20:01:21 +0100

    index.html views

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+const db = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "instrukcije"
+});
+
+db.connect(function(error){
+        if(!!error) {
+                cosole.log("Error");
+        } else {
+                console.log("Connected")
+        }
+});
 
 app.get("/", function(req,res){
         res.send('Deluje! Dogaja se! HAHA :D');
-})
+        db.query("SELECT * FROM instrukcije", function (error, rows, fields))
+        if (!!error) {
+                console.log("Error in the query");
+        } else {
+                console.log("Successful query");
+        }
+});
 
 app.listen(process.env.PORT || 5000)
